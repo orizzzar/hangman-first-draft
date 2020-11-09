4c9db6715268eaf85142efe45be15f473bc87663
orizzzar <70523219+orizzzar@users.noreply.github.com>
2020-11-09 15:59:33 +0100

    asd

--- build/app.js
+++ build/app.js
@@ -1,9 +1,15 @@
 window.addEventListener("load", init);
 const lettersInDOM = document.querySelector("#letters");
 const attemptInDOM = document.querySelector("#attempt");
-let attempts = 5;
+let attempts;
+let word;
+const guessedLetters = [];
+let words = [
+    'apple', 'window', 'chart', 'channel', 'trash'
+];
 function init() {
     writeAlphabetToTheDom();
+    newRound();
 }
 function writeAlphabetToTheDom() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
@@ -13,7 +19,56 @@ function writeAlphabetToTheDom() {
         divKey.id = element;
         divKey.classList.add("key");
         divKey.innerHTML = element;
+        divKey.addEventListener('click', () => {
+            guessLetter(element);
+        });
         keyboard.append(divKey);
     });
+}
+function newRound() {
+    document.body.classList.remove("lost");
+    attempts = 5;
+    word = getRandomArrayElement(words);
+    guessedLetters.length = 0;
+    const letters = word.split('');
+    guessedLetters.push(getRandomArrayElement(letters));
+    drawWord();
+    drawAttempts();
+}
+function drawWord() {
+    lettersInDOM.innerHTML = '';
+    const letters = word.split('');
+    letters.forEach((wordLetter) => {
+        const letterElement = document.createElement('li');
+        letterElement.innerHTML = "_";
+        guessedLetters.forEach((guessedLetter) => {
+            if (wordLetter === guessedLetter) {
+                letterElement.innerHTML = guessedLetter;
+            }
+        });
+        lettersInDOM.append(letterElement);
+    });
+}
+function drawAttempts() {
+    attemptInDOM.innerHTML = attempts.toString();
+}
+function getRandomArrayElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+function guessLetter(letter) {
+    if (word.includes(letter)) {
+        guessedLetters.push(letter);
+        drawWord();
+    }
+    else {
+        if (--attempts === 0) {
+            lose();
+        }
+        drawAttempts();
+    }
+}
+function lose() {
+    document.body.classList.add("lost");
+    setTimeout(newRound, 500);
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,14 @@ window.addEventListener("load", init);
 //Global variables
 const lettersInDOM: HTMLDivElement = document.querySelector("#letters");
 const attemptInDOM: HTMLDivElement = document.querySelector("#attempt");
-let attempts: number = 5;
+
+let attempts: number;
+let word: string;
+const guessedLetters: string[] = [];
+
+let words: string[] = [
+  'apple', 'window', 'chart', 'channel', 'trash'
+];
 
 /**
  * Function to initialize the programme
@@ -11,6 +18,7 @@ let attempts: number = 5;
 function init() {
   //write the alphabet keyboard to the DOM
   writeAlphabetToTheDom();
+  newRound();
 }
 
 /**
@@ -24,6 +32,66 @@ function writeAlphabetToTheDom() {
     divKey.id = element;
     divKey.classList.add("key");
     divKey.innerHTML = element;
+    divKey.addEventListener('click', () => {
+      guessLetter(element);
+    })
     keyboard.append(divKey);
   });
+}
+
+function newRound() {
+  document.body.classList.remove("lost");
+  attempts = 5;
+  word = getRandomArrayElement(words);
+  guessedLetters.length = 0;
+  
+  // Random letter (to begin with)
+  const letters: string[] = word.split('');
+  guessedLetters.push(getRandomArrayElement(letters));
+
+  drawWord();
+  drawAttempts();
+}
+
+function drawWord() {
+  lettersInDOM.innerHTML = '';
+  const letters: string[] = word.split('');
+
+  letters.forEach((wordLetter) => {
+    const letterElement: HTMLLIElement = document.createElement('li');
+  
+    letterElement.innerHTML = "_";
+    guessedLetters.forEach((guessedLetter) => {
+      if (wordLetter === guessedLetter) {
+        letterElement.innerHTML = guessedLetter;
+      }
+    });
+
+    lettersInDOM.append(letterElement);
+  });
+}
+
+function drawAttempts() {
+  attemptInDOM.innerHTML = attempts.toString();
+}
+
+function getRandomArrayElement(array: any[]): any {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function guessLetter(letter: string) {
+  if (word.includes(letter)) {
+    guessedLetters.push(letter);
+    drawWord();
+  } else {
+    if (--attempts === 0) {
+      lose()
+    }
+    drawAttempts();
+  }
+}
+
+function lose() {
+  document.body.classList.add("lost");
+  setTimeout(newRound, 500);
 }
